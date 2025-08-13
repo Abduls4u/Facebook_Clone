02f9be34c51425cd271ab4c54c38ad2f5cb07347
Abdulsomad <abdulsalamabdulsomad14@gmail.com>
2025-08-13 11:30:25 +0100

    Customize admin panel

--- backend/apps/posts/admin.py
+++ backend/apps/posts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Post, PostMedia, PostTag
 
-# Register your models here.
+class PostMediaInline(admin.TabularInline):
+    model = PostMedia
+    extra = 0
+
+class PostTagInline(admin.TabularInline):
+    model = PostTag
+    extra = 0
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author', 'content_preview', 'post_type', 'privacy', 'created_at']
+    list_filter = ['post_type', 'privacy', 'created_at']
+    search_fields = ['author__username', 'content']
+    inlines = [PostMediaInline, PostTagInline]
+    
+    def content_preview(self, obj):
+        return obj.content[:50] + '...' if len(obj.content) > 50 else obj.content
+    content_preview.short_description = 'Content'
+
+@admin.register(PostMedia)
+class PostMediaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'post', 'media_type', 'file', 'order']
+    list_filter = ['media_type', 'created_at']
